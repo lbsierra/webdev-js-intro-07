2b3ccb67952ff9307893ad25f20f96ce4341ffce
Sierra <sonicaradia@gmail.com>
2025-01-18 14:50:49 -0500

    Experimented with functions

--- js/index.js
+++ js/index.js
@@ -1,4 +1,46 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+function sum(a, b) {
+    let sumResult = a + b;
+    console.log(sumResult);
+}
 
+sum(4, 4);
+sum(45, 15);
+sum(9000, 1);
+
+function product(c, d) {
+    let productResult = c * d;
+    console.log(productResult);
+}
+
+product(10, 10);
+product(8, 8);
+product(9, 5);
+
+
+function rng() {
+    return Math.floor(Math.random() * 10); 
+}
+
+let randomNumber = rng();
+console.log(randomNumber);
+
+
+function greetName(name) {
+    console.log("Hello " + name);
+}
+
+greetName("Mike");
+
+
+function calculateTotal(price, quantity = 1) {
+    let total = price * quantity;
+    console.log(total);
+}
+
+calculateTotal(5.57);
+calculateTotal(9.99, 2);
+*/
